b86f1f35fe09ac568787e777b4235b623c8fd077
momofcats <ykhilko@gmail.com>
2020-01-09 16:05:13 -0800

    remove console.log's and replace with DOM methods

--- game.js
+++ game.js
@@ -1,6 +1,15 @@
 let playerScore = 0;
 let computerScore = 0;
 
+let resultsContainer = document.createElement("div");
+let bodyElement = document.querySelector("body");
+const playerChoice = document.createElement("h4");
+const computerChoice = document.createElement("h4");
+const compScore = document.createElement("h4");
+const yourScore = document.createElement("h4");
+const gameOvermessage = document.createElement("h4");
+bodyElement.appendChild(resultsContainer);
+
 const buttons = document.querySelector(".js-buttons-container");
 buttons.addEventListener("click", playRound);
 
@@ -21,40 +30,37 @@ function computerPlay() {
 function playRound(e) {
   let playerSelection = e.target.value;
   let computerSelection = computerPlay();
-  console.log("you: " + playerSelection);
-  console.log("computer: " + computerSelection);
+  showSelectons(playerSelection, computerSelection);
   if (
     (computerSelection === "paper" && playerSelection === "rock") ||
     (computerSelection === "scissors" && playerSelection === "paper") ||
     (computerSelection === "rock" && playerSelection === "scissors")
   ) {
-    console.log("You lost the round!");
     computerScore++;
   } else if (
     (computerSelection === "scissors" && playerSelection === "rock") ||
     (computerSelection === "rock" && playerSelection === "paper") ||
     (computerSelection === "paper" && playerSelection === "scissors")
   ) {
-    console.log("You won the round!");
     playerScore++;
-  } else {
-    console.log("It's a tie!");
   }
-
-  console.log("player score: " + playerScore);
-  console.log("computer score: " + computerScore);
+  compScore.textContent = "computer score: " + computerScore;
+  yourScore.textContent = "player score: " + playerScore;
+  resultsContainer.appendChild(compScore);
+  resultsContainer.appendChild(yourScore);
   game(computerScore, playerScore);
 }
 
 function game(computerScore, playerScore) {
   if (computerScore === 5 || playerScore === 5) {
     if (computerScore > playerScore) {
-      console.log("Computer won the game!");
+      gameOvermessage.textContent = "Computer won the game!";
     } else if (playerScore > computerScore) {
-      console.log("Player won the game!");
+      gameOvermessage.textContent = "Player won the game!";
     } else {
-      console.log("It's a tie!");
+      gameOvermessage.textContent = "It's a tie!";
     }
+    resultsContainer.appendChild(gameOvermessage);
     disableButtons();
   }
 }
@@ -64,4 +70,11 @@ function disableButtons() {
   for (i = 0; i < button.length; i++) {
     button[i].disabled = true;
   }
+}
+
+function showSelectons(playerSelection, computerSelection) {
+  playerChoice.textContent = "player: " + playerSelection;
+  computerChoice.textContent = "computer: " + computerSelection;
+  resultsContainer.appendChild(computerChoice);
+  resultsContainer.appendChild(playerChoice);
 }
